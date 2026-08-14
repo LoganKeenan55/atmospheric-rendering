77852cf199471cae55e5f9b9fad1c072dc3d8ce1
Logan <logankeenan55@gmail.com>
2026-08-13 22:12:47 -0400

    directionalLight3D follows shader sun, to cast shadows

--- Camera.cs
+++ Camera.cs
@@ -8,8 +8,8 @@ public partial class Camera : Camera3D
 	Vector2 lookAngles = Vector2.Zero;
 
 	[Export]
-	private float acceleration = 2500.0f;
-	private float moveSpeed = 2000.0f;
+	private float acceleration = 250000.0f;
+	private float moveSpeed = 200.0f;
 	private float mouseSpeed = 300.0f;
 
 	public override void _Ready(){
@@ -18,6 +18,18 @@ public partial class Camera : Camera3D
 	}
 
 	public override void _Process(double delta){
+
+		if (Input.IsActionJustPressed("wheelUp")){
+			moveSpeed += 100;
+		}
+		if (Input.IsActionJustPressed("wheelDown")){
+			moveSpeed -= 100;
+			if(moveSpeed < 20){
+				moveSpeed = 20;
+			}
+		}
+
+
 		lookAngles.Y = (float)Math.Clamp(lookAngles.Y, Math.PI / -2, Math.PI / 2);
 		Rotation = new Vector3(lookAngles.Y,lookAngles.X,0);
 		Vector3 direction = updateDirection(delta);
--- Main.cs
+++ Main.cs
@@ -6,15 +6,16 @@ public partial class Main : Node3D
 {
 	private Camera3D camera;
 	private WorldEnvironment worldEnvironment;
+	private DirectionalLight3D sun;
 	private Godot.Vector3 sunDir = new Godot.Vector3(0.0f,0.0f,0.0f); //for straight up (0.0f,1.0f,0.0f);
 
 	private float sunSpeed = .5f;
-	private float angle = 0.0f;
+	private float angle = 45.0f;
 
 	public override void _Ready(){
 		camera = GetNode<Camera3D>("Camera");
 		worldEnvironment = GetNode<WorldEnvironment>("WorldEnvironment");
-
+		sun = GetNode<DirectionalLight3D>("Sun");
 
 	}
 
@@ -27,7 +28,17 @@ public partial class Main : Node3D
 		}
 		sunDir.X = Mathf.Sin(angle);
 		sunDir.Y = MathF.Cos(angle);
+		sunDir.Z = 0.0f;
+		sunDir = sunDir.Normalized();
 		setSkyUniforms();
+
+		sun.LookAt(sun.GlobalPosition - sunDir, Godot.Vector3.Up);
+		if(sunDir.Y < 0.0f){
+			sun.Visible = false;
+		}
+		else{
+			sun.Visible = true;
+		}
 	}
 
 
